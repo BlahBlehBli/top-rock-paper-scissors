5880f89e32c45c6705aed4491f6ee20f69c2a26e
esteb <54190902+BlahBlehBli@users.noreply.github.com>
2024-09-15 10:19:05 -0600

    add getHumanChoice function to script.js

--- script.js
+++ script.js
@@ -3,29 +3,23 @@
 function getComputerChoice(){
   let randomInt = Math.floor(Math.random()*3);
 
-  /*
-  let result = "";
-  switch (randomInt){
-    case 0:{
-      result = "Rock";
-      break;
-    }
-    case 1:{
-      result = "Paper";
-      break;
-    }
-    case 2:{
-      result = "Scissors";
-      break;
-    }
-    default:{
-      result = "Rock";
-    }
-  }
-  return result;
-  */
-
   return (randomInt===0) ? "Rock" : 
       (randomInt===1) ? "Paper" : 
       "Scissors";
+}
+
+function getHumanChoice(){
+  let humanChoice = prompt("Hi\x21 Please, choose between Rock, Paper or Scissors");
+  while (!humanChoice || (humanChoice !== "Rock" && humanChoice !== "Paper" && humanChoice !== "Scissors")){
+    if (humanChoice === ""){
+      alert("You didn't make a selection\x21");
+    } else if (humanChoice === null){
+      alert("It's rude to close the prompt without an answer\x21");
+    } else{
+      alert("Please enter a valid choice\x21");
+    }
+    humanChoice = prompt("Hi\x21 Please, choose between Rock, Paper or Scissors");
+  }
+
+  return humanChoice;
 }
